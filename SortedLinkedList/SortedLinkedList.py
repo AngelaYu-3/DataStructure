68bbcb3f5da240cedb04495256dea5f7a61b515e
AngelaYu-3 <Angieisyu36@gmail.com>
2020-07-09 22:44:34 -0700

    added more of insert()

--- SortedLinkedList/SortedLinkedList.py
+++ SortedLinkedList/SortedLinkedList.py
@@ -2,46 +2,45 @@
 Sorted Linked List in ascending order implementation (no duplicates)
 with Node class and insert, remove header, and remove tail
 methods
+
+    TODO
+        -finish last insert condition
+        -test
 """
 
 class Node:
-    def __init__(self):
-        self.data = None
+    def __init__(self, data):
+        self.data = data
         self.next = None
 
+    def data(self):
+        print('getter method called')
+        return self
 
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.tail = None
 
     def insert(self, data):
         head = self.head
 
         if head is None:
-            n = Node()
-            n.data = data
-            n.next = None
+            n = Node(data)
             self.head = n
-            self.tail = n
-            return #don't really need return bc returning null
 
         elif (head is not None) and (head.next is None):
             if head.data > data:
-                n = Node()
-                n.data = data
+                n = Node(data)
                 n.next = self.head
                 self.head = n
-                n.next.next = None
-                self.tail = n.next
                 return
 
             else:
-                n = Node()
-                n.data = data
-                n.next = None
-                self.tail = n
+                n = Node(data)
+                self.head.next = n
                 return
+
+        else:
 
 
 def main():
