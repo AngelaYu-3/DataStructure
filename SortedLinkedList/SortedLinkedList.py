793f6c1b8b7ea2b848966dbce8da063bd72155a5
AngelaYu-3 <Angieisyu36@gmail.com>
2020-07-10 22:05:18 -0700

    test more-problems

--- SortedLinkedList/SortedLinkedList.py
+++ SortedLinkedList/SortedLinkedList.py
@@ -2,11 +2,11 @@
 Sorted Linked List in ascending order implementation (no duplicates)
 with Node class and insert, remove header, and remove tail
 methods
-
     TODO
         -finish last insert condition
         -test
 """
+import sys
 
 class Node:
     def __init__(self, data):
@@ -20,6 +20,13 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def print(self):
+        curr = self.head
+
+        while curr is not None:
+            print(curr.data)
+            curr = curr.next
 
     def insert(self, data):
         head = self.head
@@ -41,24 +48,32 @@ class LinkedList:
                 return
 
         else:
+            curr = self.head
+
+            while curr.next is not None:
+                if (curr.data < data) & (curr.next.data > data):
+                    n = Node(data)
+                    curr.next = n
+                    n.next = curr.next.next
+                    break
+                else:
+                    curr = curr.next
+
+            if curr.next is None:
+                n = Node(data)
+                curr.next = n
 
 
 def main():
     ll = LinkedList();
-    num = int(input("Enter a number: "))
-    ll.insert(num)
-    num = int(input("Enter a number: "))
-    ll.insert(num)
-    c = ll.head
-    b = ll.head.next
-    print(c.data)
-    print(b.data)
+    var = 1
+    while var == 1:
+        ch = input("Enter in a value or press 'q' to quit: ")
+        ll.insert(ch)
+        ll.print()
 
 if __name__ == '__main__':
     main()
-
-
-
 
 
 
