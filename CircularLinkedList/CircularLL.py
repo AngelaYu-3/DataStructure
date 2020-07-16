5c93003de21b01acc1912275a1dfc03b2451af99
AngelaYu-3 <Angieisyu36@gmail.com>
2020-07-16 08:55:52 -0700

    added circular linked list

--- CircularLinkedList/CircularLL.py
+++ CircularLinkedList/CircularLL.py
@@ -1,4 +1,7 @@
-
+"""
+circular linked list
+      -add method for a reversed circular linked list
+"""
 class Node:
     def __init__(self, data):
        self.data = data
@@ -9,15 +12,17 @@ class CircularLL:
         self.tail = None
 
     def print(self):
-        curr = self.tail.next
-        if self.tail.next is not None:
+        if self.tail is None:
+            print('None')
+        else:
+            curr = self.tail.next
             while(True):
                 print(curr.data)
                 curr = curr.next
                 if curr == self.tail.next:
                     break
 
-
+    #insert from tail
     def insert(self, data):
         if self.tail is None:
             n = Node(data)
@@ -28,17 +33,36 @@ class CircularLL:
             next = self.tail.next
             n.next = next
             self.tail.next = n
+            self.tail = n
 
+    #remove from head
     def remove(self):
-        
+        if self.tail is None:
+            pass
+        else:
+            if self.tail.next == self.tail:
+                self.tail = None
+            else:
+                head = self.tail.next
+                self.tail.next = head.next
+                head.next = None
 
 def main():
     cll = CircularLL()
-    cll.insert(3)
-    cll.insert(5)
-    cll.print()
-    x = cll.tail.data
-    print(x)
+    while(True):
+        user = input("insert, remove, or quit: ")
+        if user == 'insert':
+            value = int(input('Value: '))
+            cll.insert(value)
+            cll.print()
+        elif user == 'remove':
+            cll.remove()
+            cll.print()
+        elif user == 'quit':
+            break
+        else:
+            continue
+
 
 if __name__ == '__main__':
     main()
