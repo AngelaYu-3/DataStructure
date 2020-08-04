f5f029032ef957258d8cc2708bf0fe877301da81
AngelaYu-3 <Angieisyu36@gmail.com>
2020-08-03 22:27:57 -0700

    added treeHeight constructor

---
+++ TreeHeight/src/TreeHeight.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TreeHeight {
+	Scanner in = new Scanner(System.in);
+	
+	public TreeHeight() {
+		System.out.print("Enter in an integer (n) for number of nodes: ");
+		int nodesNum = in.nextInt();
+		System.out.print("Enter order of tree using values -1 to n-1 to represent the structure of the tree: ");
+		String order = in.next();
+		
+		int[] nodes = new int[nodesNum];
+		//for loop to place order string (indiv int) into created array
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
