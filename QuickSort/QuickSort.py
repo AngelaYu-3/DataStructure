0d3aea17382c151e9ccc97a50e0da1d28fa35411
AngelaYu-3 <Angieisyu36@gmail.com>
2020-08-20 13:24:49 -0700

    added switch and sort logic--errors

--- QuickSort/QuickSort.py
+++ QuickSort/QuickSort.py
@@ -1,4 +1,6 @@
 class QuickSort:
+    global switch
+
     def __init__(self):
         global list_data
         dataIn = str(input("Enter in integers separated by spaces to sort: "))
@@ -7,14 +9,29 @@ class QuickSort:
         list_data = list(data)
         print(list_data)
 
+    def switch(self, index1, index2):
+        data1 = list_data[index1]
+        data2 = list_data[index2]
+        list_data[index1] = data2
+        list_data[index2] = data1
+
     def sort(self):
         length = len(list_data)
         pivot = list_data[length - 1]
-        print(pivot)
+        i = -1
+
+        for j in range(length):
+            if list_data[j] < pivot:
+                i += 1
+                switch(j, i)
+            j += 1
+
+        switch(length-1, i)
+
 
 def main():
-   quickSort = QuickSort()
-   quickSort.sort()
+    quickSort = QuickSort()
+    quickSort.sort()
 
 if __name__ == "__main__":
     main()
