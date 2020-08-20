a681d3f579739cb991492e31099d33f18c775317
AngelaYu-3 <Angieisyu36@gmail.com>
2020-08-19 21:19:26 -0700

    priority queue?

--- NetworkPacket/process_package.py
+++ NetworkPacket/process_package.py
@@ -1,16 +1,29 @@
 from collections import namedtuple
+from queue import Queue
 
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
 Response = namedtuple("Response", ["was_dropped", "started_at"])
+BufferTime = namedtuple("BufferTime", ["final_time", "started_at"])
+
+#priority queue???--to order final_times in order to pop them accordingly in order
 
 class Buffer:
+    global finalTime
+    global time
+    time = 0;
+    size = 0
     def __init__(self, size):
         self.size = size
         self.finish_time = []
 
+    finalTime = Queue(maxsize=size)
     def process(self, request):
         # write your code here
-        return Response(False, -1)
+        if finalTime.full() is False:
+            final_time = request.arrived_at + request.time_to_process
+            finalTime.append(BufferTime(final_time, request.arrived_at))
+        else:
+            return Response(True, -1)
 
 
 def process_requests(requests, buffer):
@@ -32,6 +45,8 @@ def main():
 
     for response in responses:
         print(response.started_at if not response.was_dropped else -1)
+
+    #print(buffer.size)
 
 
 if __name__ == "__main__":
