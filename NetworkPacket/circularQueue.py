00c82241ab95a88082c88819e95a7ffee878a4fb
AngelaYu-3 <yuangela12@gmail.com>
2020-08-25 12:27:00 -0700

    circular queue working

--- NetworkPacket/circularQueue.py
+++ NetworkPacket/circularQueue.py
@@ -3,47 +3,47 @@ class CircularQueue():
     def __init__(self, size):
         self.size = size
         self.queue = [None for i in range(size)]
-        self.head = self.tail = 0
+        self.head = -1
+        self.tail = -1
 
     def enqueue(self, data):
-
-        if (self.tail + 1) % self.size == self.head:
+        if(self.tail + 1) % self.size == self.head:
             print("queue is full")
+
         else:
+            #adding in first element to empty queue
             if self.head == -1:
                 self.head = 0
 
-        if (self.tail == self.head) and (self.queue[self.tail] is not None) and (self.queue[self.head] is not None):
-            print("queue is full")
-        else:
+            self.tail = (self.tail + 1) % self.size
             self.queue[self.tail] = data
-            if self.tail != self.size - 1:
-               self.tail += 1
-            else:
-                self.tail = 0
 
     def dequeue(self):
-        if (self.tail == self.head) and (self.queue[self.tail] is None) and (self.queue[self.head] is None):
+        if self.head == -1:
             print("queue is empty")
+        elif self.head == self.tail:
+            removed = self.queue[self.head]
+            self.head = self.tail = -1
+            return removed
         else:
-            self.queue.pop(self.head)
-            if self.head != self.size - 1:
-                self.head += 1
-            else:
-                self.head = 0
+            removed = self.queue[self.head]
+            self.head = (self.head + 1) % self.size
+            return removed
 
-    def print(self, is_queue):
-        if is_queue is True:
-            for i in range(self.size):
-                if self.queue[i] is None:
-                    pass
-                else:
-                    print(self.queue[i])
+    def print(self):
+        if self.tail > self.head:
+            for i in range(self.head, self.tail + 1):
+                print(self.queue[i])
+        elif self.tail == self.head:
+            if self.tail == -1:
+                pass
+            else:
+                print(self.queue[self.tail])
         else:
             for i in range(self.head, self.size):
                 print(self.queue[i])
-
-
+            for i in range(0, self.tail + 1):
+                print(self.queue[i])
 
 def main():
     size = int(input("Size of queue: "))
@@ -55,12 +55,12 @@ def main():
         if user == "enqueue":
             data = int(input("data value: "))
             test.enqueue(data)
-            print("Tail: {} Head: {}".format(test.tail, test.head))
-            test.print(True)
+            #print("Tail: {} Head: {}".format(test.tail, test.head))
+            test.print()
         elif user == "dequeue":
-            test.dequeue()
-            print("Tail: {} Head: {}".format(test.tail, test.head))
-            test.print(False)
+            print("Removed: {}".format(test.dequeue()))
+            #print("Tail: {} Head: {}".format(test.tail, test.head))
+            test.print()
         else:
             #test.print()
             exit()
