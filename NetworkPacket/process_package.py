d7ab2dadff246266a2d66f83f21571cacfc6d837
AngelaYu-3 <yuangela12@gmail.com>
2020-08-26 09:38:09 -0700

    added networkpacket

--- NetworkPacket/process_package.py
+++ NetworkPacket/process_package.py
@@ -1,3 +1,5 @@
+# python3
+
 from collections import namedtuple
 
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
@@ -11,18 +13,25 @@ class Node:
 class Buffer:
     def __init__(self, size):
         self.size = size
-        self.finish = [self.size]
-        self.time = 0
+        self.finish = []
+        #self.time = 0
 
     def process(self, request):
-        while len(self.finish) > 0 and self.finish[0] == self.time:
+        while len(self.finish) > 0 and self.finish[0] <= request.arrived_at:
             self.finish.pop(0)
 
-        if len(self.finish) == 1:
-            self.finish[0] = request.arrived_at + request.time_to_process
-            self.time += 1
-            return Response(False, request.arrived_at)
-
+        if len(self.finish) < self.size:
+            if len(self.finish) == 0:
+                self.finish.append(request.arrived_at + request.time_to_process)
+                return Response(False, request.arrived_at)
+            else:
+                curr_index = len(self.finish) - 1
+                finish_time = self.finish[curr_index] + request.time_to_process
+                self.finish.append(finish_time)
+                started_at = self.finish[curr_index]
+                return Response(False, started_at)
+        else:
+            return Response(True, -1)
 
 def process_requests(requests, buffer):
     responses = []
@@ -41,10 +50,8 @@ def main():
     buffer = Buffer(buffer_size)
     responses = process_requests(requests, buffer)
 
-    print(len(buffer.finish))
     for response in responses:
         print(response.started_at if not response.was_dropped else -1)
-
 
 if __name__ == "__main__":
     main()
